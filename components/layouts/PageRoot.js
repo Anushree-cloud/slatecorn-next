9b3983e65cd164a1d92b29cb12d1086b21efb9f8
Anushree Chakraborty <anushree.chakraborty@codelogicx.com>
2024-10-23 14:17:59 +0530

    prefetched routes

--- components/layouts/PageRoot.js
+++ components/layouts/PageRoot.js
@@ -2,11 +2,24 @@
 import { ROUTES } from '@/constants/routes'
 import { initialSelectedItem } from '@/constants/sideNavigation'
 import { selectSidebarItem } from '@/store/reducers/sidebar'
-import { redirect } from 'next/navigation'
+import { redirect, useRouter } from 'next/navigation'
+import { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 
 export default function PageRoot() {
+	const router = useRouter()
 	const isLoggedIn = useSelector((state) => state.user.isLoggedIn)
+
+	useEffect(() => {
+		if (isLoggedIn) {
+			router.prefetch(ROUTES.MAIN_ROUTES.dashboard)
+			router.prefetch(ROUTES.MAIN_ROUTES.slatesSettings)
+			router.prefetch(ROUTES.MAIN_ROUTES.userSettings)
+			router.prefetch(ROUTES.PROFILE.user)
+			router.prefetch(ROUTES.SLATE_ROUTES.listing)
+			router.prefetch(ROUTES.SLATE_ROUTES.view)
+		}
+	}, [isLoggedIn])
 
 	return isLoggedIn ? redirect(ROUTES.MAIN_ROUTES.dashboard) : redirect(ROUTES.AUTH_ROUTES.login)
 }
